50396131f4f7069a7b0348cfe7e7d82983d8446b
s-walrus <swalrus@yandex.ru>
2019-11-07 19:04:14 +0300

    fixed a bug

--- hex2color.lua
+++ hex2color.lua
@@ -1,4 +1,4 @@
 local function Color(hex, value)
-	return {tonumber(string.sub(hex, 2, 3), 16)/256, tonumber(string.sub(hex, 4, 5), 16)/256, tonumber(string.sub(hex, 6, 6), 16)/256, value or 1}
+	return {tonumber(string.sub(hex, 2, 3), 16)/256, tonumber(string.sub(hex, 4, 5), 16)/256, tonumber(string.sub(hex, 6, 7), 16)/256, value or 1}
 end
 return Color
